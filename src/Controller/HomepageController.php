3bf5ed178aee67d640faa71a8b55490695103171
sllim637 <slim.hammami1977>
2021-06-12 22:43:20 +0100

    sign up controller

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ *  @Route("/Home",name="homepageMainRoute")
+ */
 class HomepageController extends AbstractController
 {
     /**
@@ -15,6 +18,15 @@ class HomepageController extends AbstractController
     {
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
+        ]);
+    }
+
+    /**
+     * @Route("/sign up/",name="creation de compte")
+     */
+    public function signUp(){
+        return $this->render('homepage/signUp.html.twig', [
+
         ]);
     }
 }
