c2c317511d33175882a3d08cac7b3d022048924f
Mohamed <mouhasahss@gmail.com>
2021-08-18 12:17:29 +0100

    corrections

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -10,7 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 class UserType extends AbstractType
 {
